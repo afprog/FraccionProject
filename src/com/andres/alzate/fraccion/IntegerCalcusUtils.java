612faba956e92fa487395de58c26fbd594ce356e
unknown <andresalzate@seven4n.com>
2016-05-24 21:09:37 -0500

    add modified files

--- src/com/andres/alzate/fraccion/IntegerCalcusUtils.java
+++ src/com/andres/alzate/fraccion/IntegerCalcusUtils.java
@@ -4,4 +4,11 @@ package com.andres.alzate.fraccion;
  * Created by AfprogWin on 23/05/2016.
  */
 public class IntegerCalcusUtils {
+    public static int mcd(int a, int b){
+        return b == 0 ? a : mcd(b, a % b);
+    }
+
+    public static int mcm(int a, int b) {
+        return a * b / mcd(a, b);
+    }
 }
--- test/FraccionTest.java
+++ test/FraccionTest.java
@@ -1,5 +1,36 @@
+import com.andres.alzate.fraccion.Fraccion;
+import javaslang.control.Option;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
+
 /**
  * Created by AfprogWin on 23/05/2016.
  */
 public class FraccionTest {
+
+    private final Option<Integer> NUMERADOR = Option.of(1);
+    private final Option<Integer> DENOMINADOR = Option.of(2);
+
+    @Test(expected = IllegalArgumentException.class)
+    public void craateFraccionWithDenominatorZeroTest(){
+        Fraccion fraccion = new Fraccion(Option.of(1),Option.of(0));
+    }
+
+    @Test
+    public void craateFraccionTest(){
+        Fraccion fraccion = new Fraccion(Option.of(1),Option.of(2));
+        assertThat(fraccion.getNumerador(),equalTo(NUMERADOR));
+        assertThat(fraccion.getDenominador(),equalTo(DENOMINADOR));
+    }
+
+    @Test
+    public void craateFraccionAndReduceTest(){
+        Fraccion fraccionToReduce = new Fraccion(Option.of(6),Option.of(4));
+        Fraccion fraccion = fraccionToReduce.reduce();
+        assertThat(fraccion.getNumerador(),equalTo(Option.of(3)));
+        assertThat(fraccion.getDenominador(),equalTo(Option.of(2)));
+    }
+
 }
